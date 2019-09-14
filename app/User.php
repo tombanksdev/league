f3951756cdb0dc0061a4409c5b5d97f7f3a45dd1
Thomas Banks <stream@tombanks.me>
2019-09-14 17:22:22 +0100

    Installed laravel passport

--- app/User.php
+++ app/User.php
@@ -2,13 +2,16 @@
 
 namespace App;
 
+use Laravel\Passport\HasApiTokens;
+
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+
+    use HasApiTokens, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -36,4 +39,5 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    
 }
